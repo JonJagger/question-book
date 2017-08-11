87fb6820ec40937f38d352cd4818e9b88b931fe4
Jon Jagger <jon@jaggersoft.com>
2017-08-11 16:18:42 +0530

    refactoring

--- app/src/public/js/question-book.js
+++ app/src/public/js/question-book.js
@@ -5,41 +5,29 @@ const ed = function(node) {
     node.prop('disabled', true);
     return node;
   };
+
   node.enable = function() {
     node.prop('disabled',false);
     return node;
   };
 
+  node.enableIf = function(tf) {
+    if (tf) {
+      node.enable();
+    } else {
+      node.disable();
+    }
+    return node;
+  };
+
+  node.disableIf = function(tf) {
+    if (tf) {
+      node.disable();
+    } else {
+      node.enable();
+    }
+    return node;
+  };
+
   return node;
 };
-
-var questionBook = (function(qb) {
-  "use strict";
-
-  qb.enable = function(node) {
-    node.prop('disabled', false);
-  };
-
-  qb.disable = function(node) {
-    node.prop('disabled', true);
-  };
-
-  qb.enableIf = function(node, tf) {
-    if (tf) {
-      qb.enable(node);
-    } else {
-      qb.disable(node);
-    }
-  };
-
-  qb.disableIf = function(node, tf) {
-    if (tf) {
-      qb.disable(node);
-    } else {
-      qb.enable(node);
-    }
-  };
-
-  return qb;
-
-})(questionBook || {});
