df6c6e4e30cb43076dc95a6881bea377ccd45bfc
Jon Jagger <jon@jaggersoft.com>
2018-08-05 09:29:33 +0100

    allow local_pipe.sh script to pass in specific port number

--- sh/local_pipe.sh
+++ sh/local_pipe.sh
@@ -2,7 +2,11 @@
 set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+source ${MY_DIR}/../.env
+readonly PORT=${1:-${QUESTION_BOOK_PORT}}
+
 ${MY_DIR}/build.sh
 ${MY_DIR}/down.sh
-${MY_DIR}/up.sh
+${MY_DIR}/up.sh ${PORT}
 ${MY_DIR}/test.sh
+echo "Up on port ${PORT}"
